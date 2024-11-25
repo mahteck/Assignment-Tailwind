35a08d54599a5a3c20db3943b0efb56f1d573e9d
Mah Teck <mahteckteach@gmail.com>
2024-11-25 18:06:37 +0500

    on lg all three blocks should be horizontal align in a single row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@
 export default function Home() {
   return (
 
-    <div className="flex flex-col items-center justify-center md:items-stretch md:flex-wrap lg: flex-nowrap">
-      <div className="bg-red-500 text-white h-[160px] w-full lg:h-80">Shoaib Munir, Software Engineer.</div>
+    <div className="flex flex-col items-center justify-center md:items-stretch md:flex-wrap lg:flex-row lg:w-full">
+      <div className="bg-red-500 text-white h-[160px] w-full lg:h-80 lg:flex-1">Shoaib Munir, Software Engineer.</div>
 
-      <div className="flex flex-col md:flex-row w-full md:w-full md:justify-between">
-        <div className="bg-blue-500 text-white h-40 w-full md:h-80 md:w-1/2">Shoaib Munir, Software Engineer.</div>
-        <div className="bg-green-500 text-white h-40 w-full md:h-80 md:w-1/2">Shoaib Munir, Software Engineer.</div>
+      <div className="flex flex-col md:flex-row w-full md:w-full lg:flex-1 lg:flex-row lg:w-auto">
+        <div className="bg-blue-500 text-white h-40 w-full md:h-80 lg:h-80 lg:flex-1">Shoaib Munir, Software Engineer.</div>
+        <div className="bg-green-500 text-white h-40 w-full md:h-80 lg:h-80 lg:flex-1">Shoaib Munir, Software Engineer.</div>
       </div>
 
     </div>
